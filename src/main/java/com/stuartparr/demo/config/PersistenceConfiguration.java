876473978150ea1c0ba7d2add31fc9fa61c2ae62
parrsw1 <stuartparr@gmail.com>
2020-02-16 19:58:11 -0500

    removed hardcoded config

--- src/main/java/com/stuartparr/demo/config/PersistenceConfiguration.java
+++ src/main/java/com/stuartparr/demo/config/PersistenceConfiguration.java
@@ -8,14 +8,14 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://192.168.99.100:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-
-        System.out.println("custom datasource bean has been initialized and set");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://192.168.99.100:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//
+//        System.out.println("custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
 }
